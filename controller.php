58d3199f284d67139ba033b25c62906ec5d8c7c5
mejiasbreanb1 <mejiasbreanb1@email.arizona.edu>
2016-11-22 16:40:33 -0700

    Update controller.php

--- controller.php
+++ controller.php
@@ -1,39 +1,46 @@
 <?php
-// Author: Chris Peterson
+// This file contains a bridge between the view and the model and redirects back to the proper page
+// with after processing whatever form this codew absorbs. This is the C in MVC, the Controller.
+//
+// Authors: Rick Mercer and Hassanain Jamal
+// 
+// TODO: Add control the new expected behavior to
+// register
+// log in
+// flag one quote
+// unflag all quotes
+// log out
+//
+require_once './DataBaseAdaptor.php';
 
-require_once('./DataBaseAdaptor.php');
-$DataBase = new DatabaseAdaptor();
+$pwd = $_POST['pwd'];
+$hashed_pwd = password_hash($pwd, PASSWORD_DEFAULT);
 
-if (isset($_GET['quote']) && isset($_GET['author'])) {
-    $DataBase->add($_GET['quote'], $_GET['author']);
+function password_verify($pwd, $hash){
+    $pwd = $_POST['pwd'];
+    $hash = password_hash($pwd, PASSWORD_DEFAULT);
+
+    echo password_verify('A' . $pwd, $hash) . PHP_EOL;
+    echo password_verify($pwd, $hash) . PHP_EOL;
 }
 
-
-if (isset($_GET ['mode'])) {
-    if ($_GET['mode'] == "new") {
-        header("Location: index.php?mode=new");
-    } else if ($_GET['mode'] == "show") {
-        header("Location: index.php");
-    } else if ($_GET['mode'] == "addPoint") {
-        $DataBase->changePoint($_GET['id'], 1);
-        header("Location: index.php");
-    } else if ($_GET['mode'] == "subtractPoint") {
-        $DataBase->changePoint($_GET['id'], -1);
-        header("Location: index.php");
-    }
-} else {
-    header("Location: index.php");
+if (isset ( $_POST ['author'] ) && isset ( $_POST ['quote'] )) {
+	$author = $_POST ['author'];
+	$quote = $_POST ['quote'];
+	$myDatabaseFunctions->addNewQuote ( $quote, $author );
+	header ( "Location: ./index.php?mode=showQuotes" );
+} elseif (isset ( $_POST ['action'] ) && isset ( $_POST ['ID'] )) {
+	$action = $_POST ['action'];
+	$ID = $_POST ['ID'];
+	if ($action === 'increase') {
+		$myDatabaseFunctions->raiseRating ( $ID );
+	}
+	if ($action === 'decrease') {
+		$myDatabaseFunctions->lowerRating ( $ID );
+	}
+	if ($action === 'flag') {
+		$myDatabaseFunctions->flag ( $ID );
+	}
+	  header ( "Location: ./index.php?mode=showQuotes" );
 }
-
-
-/*
-$array = $DataBase->getQuotesAsArray();
-$record = $array[$_GET['id']-1];
-$record['points'] = $record['points']+1;
-$DataBase->changePoint($_GET['id'], $record['points']);
-
-$array = $DataBase->getQuotesAsArray();
-$record = $array[$_GET['id']-1];
-$record['points'] = $record['points']-1;
-        $DataBase->changePoint($_GET['id'], $record['points']);
-*/
+?>
